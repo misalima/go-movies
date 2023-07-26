714b2bff3afb6b30379f1738c9be39057bf1e29f
Misael Lima <misael.alisson14@gmail.com>
2023-07-25 21:26:10 -0300

    manage-catalogue component

--- src/components/ManageCatalogue.jsx
+++ src/components/ManageCatalogue.jsx
@@ -1,9 +1,69 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useOutletContext } from "react-router-dom";
+
+
 const ManageCatalogue = () => {
+    const [movies, setMovies] = useState([]);
+    const { jwtToken } = useOutletContext();
+    const navigate = useNavigate();
+    
+    useEffect( () => {
+        if (jwtToken === "") {
+            navigate("/login");
+            return
+        }
+
+        const headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        headers.append("Authorization", "Bearer " + jwtToken);
+        
+        const requestOptions = {
+            methor: "GET",
+            headers: headers
+        }
+
+        fetch(`http://localhost:8080/admin/movies`, requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                setMovies(data);
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+    }, [jwtToken, navigate]);
+
     return (
         <div>
-            <h2>ManageCatalogue</h2>
+            <h2>Manage Catalogue</h2>
             <hr />
-            
+            <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Movie</th>
+                        <th>Release Date</th>
+                        <th>Rating</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {movies.map((m) => (
+                        <tr key={m.id}>
+                            <td>
+                                <Link to={`/admin/movies/${m.id}`}>
+                                    {m.title}
+                                </Link>
+                            </td>
+                            <td>
+                                {m.release_date}
+                            </td>
+                            <td>
+                                {m.mpaa_rating}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
