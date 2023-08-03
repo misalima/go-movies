6f0e9ce7e30227cf0eec162f2c4ecdaac5fc5465
Misael Lima <misael.alisson14@gmail.com>
2023-08-02 21:43:10 -0300

    setting movies display in movie component

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -7,23 +7,45 @@ const Movie = () => {
 
 
     useEffect( () => {
-        let myMovie = {
-            id: 1,
-            title: "Highlander",
-            release_date: "1986-03-07",
-            runtime: 116,
-            mpaa_rating: "R",
-            description: "Some long description",
-        }
+       const headers = new Headers();
+       headers.append("Content-Type", "application/json");
 
-        setMovie(myMovie);
+       const requestOptions = {
+        method: "GET",
+        headers: headers,
+       }
+
+       fetch(`http://localhost:8080/movies/${id}`, requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+            setMovie(data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+        
     }, [id]);
 
-    return (
+    if(movie.genres) {
+        movie.genres = Object.values(movie.genres);
+    } else {
+        movie.genres = []
+    }
+
+    return ( 
         <div>
             <h2>{movie.title}</h2>
-            <small><em>{movie.release_date}, {movie.runtime} minutes, Rated {movie.mpaa_rating}</em></small>
+            <small><em>{movie.release_date}, {movie.runtime} minutes, Rated {movie.mpaa_rating}</em></small><br />
+            {movie.genres.map((g) => (
+                <span key={g.genre} className="badge bg-secondary me-2">{g.genre}</span>
+            ))}
             <hr />
+            {movie.image !== "" &&
+                <div className="mb-3">
+                    <img src={`https://image.tmdb.org/t/p/w200/${movie.image}`} alt="poster" />
+                </div>    
+            }
             <p>{movie.description}</p>
         </div>
     );
